679c48d8edd0811b77537aad9924e847b95a035d
Luis Eduardo Brito <luiseduardo14@gmail.com>
2015-09-06 22:26:49 -0300

    Feat: Criando os planos de bits para o ex2 mas há problemas nomeando arquivos

---
+++ ex2/bitplanes.m
@@ -0,0 +1,18 @@
+#! /usr/local/bin/octave -qf
+
+addpath('./');
+arg_list = argv ();
+
+input = arg_list{1};
+output = arg_list{2};
+
+input_img = imread(input);
+d_input_img = double(input_img);
+
+for i = 1:8
+
+  img_bitplane = bitget(input_img, i);
+  filename = strcat(strcat("bitplane_", i), ".pgm");
+  imwrite(img_bitplane, strcat(output, filename));
+
+  endfor
---
+++ ex2/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+chmod +x bitplanes.m
+./bitplanes.m ../images/peppers.png results/;
+echo "Result images created at ./results";
